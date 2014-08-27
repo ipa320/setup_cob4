66c11247095b32e859075f881bfa35254bac01f5
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2014-08-27 12:42:59 +0200

    install upstart

---
+++ upstart/upstart_install.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+if [ "$HOSTNAME" != "$ROBOT-b1" ]; then 
+	echo "FATAL: CAN ONLY BE EXECUTED ON BASE PC"
+	exit
+fi
+
+
+sudo cp /u/robot/git/setup_cob4/upstart/cob.conf /etc/init/cob.conf
+sudo cp /u/robot/git/setup_cob4/upstart/cob-start /usr/sbin/cob-start
+sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
+sudo cp /u/robot/git/setup_cob4/upstart/cob-stop /usr/sbin/cob-stop
+	
+
+client_list="
+$ROBOT-b1
+$ROBOT-t1
+$ROBOT-t2
+$ROBOT-t3
+$ROBOT-s1
+$ROBOT-h1"
+
+for client in $client_list; do
+	echo "-------------------------------------------"
+	echo "Executing on $client"
+	echo "-------------------------------------------"
+	echo ""
+	ssh $client "sudo mkdir -p /etc/ros/hydro/cob.d"
+	ssh $client "sudo ln -s /u/robot/git/setup_cob4/upstart/cob.d/setup /etc/ros/hydro/cob.d/setup"
+	ret=${PIPESTATUS[0]}
+	if [ $ret != 0 ] ; then
+		echo "command return an error (error code: $ret), aborting..."
+		exit 1
+	fi
+	echo ""
+done
+
+sudo cp -r /u/robot/git/setup_cob4/upstart/cob.d/launch /etc/ros/hydro/cob.d/launch
+sudo sed -i "s/myrobot/$ROBOT/g" /etc/ros/hydro/cob.d/robot/robot.launch
+
+
