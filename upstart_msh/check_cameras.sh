60987a2b3d8bdeefda75c1f5c133bf4fb7cb1db2
ipa-nhg <nadia.hammoudeh.garcia@ipa.fraunhofer.de>
2016-10-11 17:37:29 +0200

    test the check_cameras script on the robot

--- upstart_msh/check_cameras.sh
+++ upstart_msh/check_cameras.sh
@@ -1,9 +1,44 @@
 #!/bin/bash
 sleep 5
+check1=false
+check2=false
+check3=false
+check4=false
+
+echo "$(date)"
+
 dmesg | grep "Not enough bandwidth for new device state."
 if [ $? -eq 0 ]; then
-  echo "error found"
-  sudo reboot -f now
+  echo "error -- Not enough bandwidth -- found"
 else
-  touch /tmp/check_done
+  check1=true
+fi
+
+dmesg | grep "cannot get min/max values"
+if [ $? -eq 0 ]; then
+  echo "error -- cannot get min/max value -- found"
+else
+  check2=true
+fi
+
+dmesg | grep "cannot get freq at ep 0x84"
+if [ $? -eq 0 ]; then
+  echo "error -- cannot get freq -- found" 
+else
+  check3=true
+fi
+
+dmesg | grep "device descriptor read/all, error -110"
+if [ $? -eq 0 ]; then
+  echo "error -- device descriptor read/all -- found"
+else
+  check4=true
+fi
+
+
+if $check1 && $check2 && $check3 && $check4; then
+ echo "OK"
+ touch /tmp/check_done
+else
+ reboot -f now
 fi
