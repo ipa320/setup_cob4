16800e4f4032045cadc7d9fb3a6cfd8e523c1818
robot <cob4-7@ipa.fhg.de>
2017-01-25 09:53:04 +0100

    added command to stop and start virtual machine

--- upstart_msh/upstart_install.sh
+++ upstart_msh/upstart_install.sh
@@ -2,13 +2,25 @@
 
 robot_name="${HOSTNAME//-b1}"
 ROS_DISTRO=indigo
+
 sudo cp /u/robot/git/setup_cob4/upstart_msh/cob.conf /etc/init/cob.conf
 sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-start /usr/sbin/cob-start
 sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-start
-sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
-sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop
 sudo sed -i "s/mydistro/$ROS_DISTRO/g" /usr/sbin/cob-start
+sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
+echo "%users ALL=NOPASSWD:/usr/sbin/cob-start" | sudo tee -a /etc/sudoers
+
 sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-stop /usr/sbin/cob-stop
+sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop
+echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop" | sudo tee -a /etc/sudoers
+
+sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-start-msh-gui /usr/sbin/cob-start-msh-gui
+sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-start-msh-gui
+echo "%users ALL=NOPASSWD:/usr/sbin/cob-start-msh-gui" | sudo tee -a /etc/sudoers
+
+sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-stop-msh-gui /usr/sbin/cob-stop-msh-gui
+sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop-msh-gui
+echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop-msh-gui" | sudo tee -a /etc/sudoers
 
 client_list="
 $robot_name-b1
@@ -48,5 +60,3 @@ done
 
 sudo cp -r /u/robot/git/setup_cob4/upstart_msh/cob.d /etc/ros/$ROS_DISTRO/.
 sudo sed -i "s/myrobot/$ROBOT/g" /etc/ros/$ROS_DISTRO/cob.d/launch/robot/robot.launch
-
-
