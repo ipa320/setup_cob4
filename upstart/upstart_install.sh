32b1d8a0c7daf76fa71156a0faba02b0d0f6f7a1
ipa-nhg <nhg@ipa-fhg.de>
2017-02-08 13:53:21 +0100

    simplify install upstart installation

--- upstart/upstart_install.sh
+++ upstart/upstart_install.sh
@@ -15,30 +15,9 @@ sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop-core" | sudo tee -a /etc/sudoe
 
 	
 
-client_list="
-$robot_name-b1
-$robot_name-t1
-$robot_name-t2
-$robot_name-t3
-$robot_name-s1
-$robot_name-h1"
-
-for client in $client_list; do
-	echo "-------------------------------------------"
-	echo "Executing on $client"
-	echo "-------------------------------------------"
-	echo ""
-	ssh $client "sudo mkdir -p /etc/ros/$ROS_DISTRO/cob.d"
-	ssh $client "sudo ln -s /u/robot/git/setup_cob4/upstart/cob.d/setup /etc/ros/$ROS_DISTRO/cob.d/setup"
-	ret=${PIPESTATUS[0]}
-	if [ $ret != 0 ] ; then
-		echo "command return an error (error code: $ret), aborting..."
-	fi
-	echo ""
-done
 
 camera_client_list="
-$robot_name-t2
+$robot_name-t1
 $robot_name-t3
 $robot_name-s1"
 
@@ -51,7 +30,7 @@ for client in $camera_client_list; do
         ssh $client "sudo update-rc.d check_cameras.sh defaults"
 done
 
-sudo cp -r /u/robot/git/setup_cob4/upstart/cob.d/launch /etc/ros/$ROS_DISTRO/cob.d/
+sudo mkdir /etc/ros/$ROS_DISTRO
+sudo cp -rf /u/robot/git/setup_cob4/upstart/cob.d /etc/ros/$ROS_DISTRO/cob.d
 sudo sed -i "s/myrobot/$ROBOT/g" /etc/ros/$ROS_DISTRO/cob.d/launch/robot/robot.launch
-sudo sed -i "s/myrobot/$robot_name/g" /etc/ros/indigo/cob.d/setup/setup.sh
-
+sudo sed -i "s/myrobotname/$robot_name/g" /etc/ros/$ROS_DISTRO/cob.d/setup/setup.sh
