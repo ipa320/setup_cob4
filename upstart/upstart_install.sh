0f6b5276830e5cc4451847900c7cc053ea54a8e0
ipa-nhg <nhg@ipa.fhg.de>
2017-04-07 10:02:30 +0200

    update install scripts

--- upstart/upstart_install.sh
+++ upstart/upstart_install.sh
@@ -3,39 +3,23 @@
 robot_name="${HOSTNAME//-b1}"
 
 sudo apt-get install ros-indigo-robot-upstart
-sudo cp /u/robot/git/setup_cob4/upstart/cob.conf /etc/init/cob.conf
+sudo apt-get install tmux
+sudo apt-get install nmap
+
 sudo cp /u/robot/git/setup_cob4/upstart/cob-start /usr/sbin/cob-start
-sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
+sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-start
 sudo sed -i "s/mydistro/$ROS_DISTRO/g" /usr/sbin/cob-start
-sudo cp /u/robot/git/setup_cob4/upstart/cob-stop /usr/sbin/cob-stop
-sudo cp /u/robot/git/setup_cob4/upstart/cob-stop-core /usr/sbin/cob-stop-core
-sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-start" | sudo tee -a /etc/sudoers
-sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop" | sudo tee -a /etc/sudoers
-sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop-core" | sudo tee -a /etc/sudoers
+sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
+sudo sed -i "s/myuser/robot/g" /usr/sbin/cob-start
+echo "%users ALL=NOPASSWD:/usr/sbin/cob-start" | sudo tee -a /etc/sudoers
 
-	
+sudo cp /u/robot/git/setup_cob4/scripts/cob-stop /usr/sbin/cob-stop
+sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop
+sudo sed -i "s/myuser/msh/g" /usr/sbin/cob-stop
+echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop" | sudo tee -a /etc/sudoers
 
-client_list="
-$robot_name-b1
-$robot_name-t1
-$robot_name-t2
-$robot_name-t3
-$robot_name-s1
-$robot_name-h1"
-
-for client in $client_list; do
-	echo "-------------------------------------------"
-	echo "Executing on $client"
-	echo "-------------------------------------------"
-	echo ""
-	ssh $client "sudo mkdir -p /etc/ros/$ROS_DISTRO/cob.d"
-	ssh $client "sudo ln -s /u/robot/git/setup_cob4/upstart/cob.d/setup /etc/ros/$ROS_DISTRO/cob.d/setup"
-	ret=${PIPESTATUS[0]}
-	if [ $ret != 0 ] ; then
-		echo "command return an error (error code: $ret), aborting..."
-	fi
-	echo ""
-done
+sudo cp /u/robot/git/setup_cob4/scripts/cob-command /usr/sbin/cob-command
+sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-command" | sudo tee -a /etc/sudoers
 
 camera_client_list="
 $robot_name-t2
@@ -50,8 +34,3 @@ for client in $camera_client_list; do
         ssh $client "sudo cp /u/robot/git/setup_cob4/upstart/check_cameras.sh /etc/init.d/check_cameras.sh"
         ssh $client "sudo update-rc.d check_cameras.sh defaults"
 done
-
-sudo cp -r /u/robot/git/setup_cob4/upstart/cob.d/launch /etc/ros/$ROS_DISTRO/cob.d/
-sudo sed -i "s/myrobot/$ROBOT/g" /etc/ros/$ROS_DISTRO/cob.d/launch/robot/robot.launch
-sudo sed -i "s/myrobot/$robot_name/g" /etc/ros/indigo/cob.d/setup/setup.sh
-
--- upstart_msh/upstart_install.sh
+++ upstart_msh/upstart_install.sh
@@ -6,45 +6,16 @@ sudo cp /u/robot/git/setup_cob4/upstart_msh/cob.conf /etc/init/cob.conf
 sudo cp /u/robot/git/setup_cob4/upstart_msh/cob_msh.conf /etc/init/cob_msh.conf
 sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-start /usr/sbin/cob-start
 sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-start
-sudo sed -i "s/mydistro/$ROS_DISTRO/g" /usr/sbin/cob-start
+sudo sed -i "s/mydistro/indigo/g" /usr/sbin/cob-start
 sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
 sudo sed -i "s/myuser/msh/g" /usr/sbin/cob-start
 echo "%users ALL=NOPASSWD:/usr/sbin/cob-start" | sudo tee -a /etc/sudoers
 
-sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-stop /usr/sbin/cob-stop
-sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop
-sudo sed -i "s/myuser/msh/g" /usr/sbin/cob-stop
+sudo cp /u/robot/git/setup_cob4/scripts/cob-stop /usr/sbin/cob-stop
 echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop" | sudo tee -a /etc/sudoers
 
-sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-start-gui /usr/sbin/cob-start-gui
-sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-start-gui
-echo "%users ALL=NOPASSWD:/usr/sbin/cob-start-gui" | sudo tee -a /etc/sudoers
-
-sudo cp /u/robot/git/setup_cob4/upstart_msh/cob-stop-gui /usr/sbin/cob-stop-gui
-sudo sed -i "s/myrobotname/$robot_name/g" /usr/sbin/cob-stop-gui
-echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop-gui" | sudo tee -a /etc/sudoers
-
-client_list="
-$robot_name-b1
-$robot_name-t1
-$robot_name-t2
-$robot_name-t3
-$robot_name-s1
-$robot_name-h1"
-
-for client in $client_list; do
-	echo "-------------------------------------------"
-	echo "Executing on $client"
-	echo "-------------------------------------------"
-	echo ""
-	ssh $client "sudo mkdir -p /etc/ros/$ROS_DISTRO/cob.d"
-	ssh $client "sudo ln -s /u/robot/git/setup_cob4/upstart_msh/cob.d/setup /etc/ros/$ROS_DISTRO/cob.d/setup"
-	ret=${PIPESTATUS[0]}
-	if [ $ret != 0 ] ; then
-		echo "command return an error (error code: $ret), aborting..."
-	fi
-	echo ""
-done
+sudo cp /u/robot/git/setup_cob4/scripts/cob-command /usr/sbin/cob-command
+sudo echo "%users ALL=NOPASSWD:/usr/sbin/cob-command" | sudo tee -a /etc/sudoers
 
 camera_client_list="
 $robot_name-t2
@@ -60,5 +31,3 @@ for client in $camera_client_list; do
         ssh $client "sudo update-rc.d check_cameras.sh defaults"
 done
 
-sudo cp -r /u/robot/git/setup_cob4/upstart_msh/cob.d /etc/ros/$ROS_DISTRO/.
-sudo sed -i "s/myrobot/$ROBOT/g" /etc/ros/$ROS_DISTRO/cob.d/launch/robot/robot.launch
