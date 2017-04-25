92331bb668bc96f9bfd58d9d682d54d9746a0f2a
ipa-nhg <nhg@ipa.fhg.de>
2017-04-25 18:45:49 +0200

    clean test asus files

--- upstart/check_cameras.sh
+++ upstart/check_cameras.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+# during the test some extra dmesg errors were found but we can ignore then to start succesfully the driver:
+#ERROR1 : cannot get freq at ep 0x84
+#ERROR2 : cannot set freq 44100 to ep 0x84
+
 sleep 30
 check1=false
 check2=false
-check3=true
-check4=false
-check5=true
+check3=false
 
 echo "$(date)"
 
@@ -22,28 +24,14 @@ else
   check2=true
 fi
 
-#dmesg | grep "cannot get freq at ep 0x84"
-#if [ $? -eq 0 ]; then
-#  echo "error -- cannot get freq -- found" 
-#else
-#  check3=true
-#fi
-
 dmesg | grep "device descriptor read/all, error -110"
 if [ $? -eq 0 ]; then
   echo "error -- device descriptor read/all -- found"
 else
-  check4=true
+  check3=true
 fi
 
-#dmesg | grep "cannot set freq 44100 to ep 0x84"
-#if [ $? -eq 0 ]; then
-#  echo "error -- cannot set freq 44100 to ep 0x84 -- found"
-#else
-#  check5=true
-#fi
-
-if $check1 && $check2 && $check3 && $check4 && $check5; then
+if $check1 && $check2 && $check3; then
  echo "OK"
  touch /tmp/check_done
 else
