f445f0eafbc80e1cdff4bafae22d9f11a6ff5364
ipa-nhg <nhg@ipa-fhg.de>
2017-06-30 17:17:47 +0200

    merging tested cob-command into master

--- upstart/upstart_install.sh
+++ upstart/upstart_install.sh
@@ -2,29 +2,32 @@
 
 set -e
 
+robot_name="${HOSTNAME//-b1}"
+
+sudo apt-get install ros-indigo-robot-upstart
+sudo apt-get install tmux
+sudo apt-get install nmap
+
 sudo cp -f /u/robot/git/setup_cob4/upstart/cob.conf /etc/init/cob.conf
 sudo cp -f /u/robot/git/setup_cob4/upstart/cob-start /usr/sbin/cob-start
-sudo sed -i "s/myrobot/$ROBOT/g" /usr/sbin/cob-start
+sudo sed -i "s/myrobot/$robot_name/g" /usr/sbin/cob-start
+
+sudo cp -f /u/robot/git/setup_cob4/upstart/cob-stop /usr/sbin/cob-stop
+
+sudo cp -f /u/robot/git/setup_cob4/upstart/cob.yaml /etc/ros/cob.yaml
+sudo sed -i "s/myrobot/$robot_name/g" /etc/ros/cob.yaml
 
 sudo cp -f /u/robot/git/setup_cob4/scripts/cob-command /usr/sbin/cob-command
 
-sudo cp -f /u/robot/git/setup_cob4/upstart/cob.yaml /etc/ros/cob.yaml
-sudo sed -i "s/myrobot/$ROBOT/g" /etc/ros/cob.yaml
+sudo sh -c 'echo "%users ALL=NOPASSWD:/usr/sbin/cob-start"' | sudo sed -i -e "\|%users ALL=NOPASSWD:/usr/sbin/cob-start|h; \${x;s|%users ALL=NOPASSWD:/usr/sbin/cob-start||;{g;t};a\\" -e "%users ALL=NOPASSWD:/usr/sbin/cob-start" -e "}" /etc/sudoers 
+sudo sh -c 'echo "%users ALL=NOPASSWD:/usr/sbin/cob-stop"' | sudo sed -i -e "\|%users ALL=NOPASSWD:/usr/sbin/cob-stop|h; \${x;s|%users ALL=NOPASSWD:/usr/sbin/cob-stop||;{g;t};a\\" -e "%users ALL=NOPASSWD:/usr/sbin/cob-stop" -e "}" /etc/sudoers 
+sudo sh -c 'echo "%users ALL=NOPASSWD:/usr/sbin/cob-command"' | sudo sed -i -e "\|%users ALL=NOPASSWD:/usr/sbin/cob-command|h; \${x;s|%users ALL=NOPASSWD:/usr/sbin/cob-command||;{g;t};a\\" -e "%users ALL=NOPASSWD:/usr/sbin/cob-command" -e "}" /etc/sudoers 
 
-sudoers_string="%users ALL=NOPASSWD:/usr/sbin/cob-start"
-if ! sudo grep -q "$sudoers_string" /etc/sudoers ; then
-  echo $sudoers_string | sudo tee -a /etc/sudoers
-fi
-sudoers_string="%users ALL=NOPASSWD:/usr/sbin/cob-command"
-if ! sudo grep -q "$sudoers_string" /etc/sudoers ; then
-  echo $sudoers_string | sudo tee -a /etc/sudoers
-fi
-
-# define ASUS camera pcs
+# TODO: make this a robot specific configuration in a yaml file
 camera_client_list="
-$ROBOT-t1
-$ROBOT-t3
-$ROBOT-s1"
+$robot_name-t1
+$robot_name-t3
+$robot_name-s1"
 
 for client in $camera_client_list; do
         echo "-------------------------------------------"
