f9e0496a7fc2dd7a11b9bbd7ecfe7fb172e1f698
ipa-nhg <nhg@ipa-fhg.de>
2017-08-08 15:06:31 +0200

    fix for composition of client_list in cob_pc scripts

--- cob-pcs/cob-pcs-execute.sh
+++ cob-pcs/cob-pcs-execute.sh
@@ -10,8 +10,8 @@ if [ "$#" -lt 1 ]; then
 fi
 
 # get pcs in local network
-IP=$(`hostname -I | awk '{print $1}'`)
-client_list=$(nmap --unprivileged $IP-98 --system-dns | grep report | awk '{print $6}' | sed 's/(//g;s/)//g')
+IP=$(hostname -I | awk '{print $1}')
+client_list=$(nmap --unprivileged $IP-98 --system-dns | grep report | awk '{print $6}' | sed 's/(//g;s/)//g' | tr '\n' ' ')
 
 for client in $client_list; do
 	echo "-------------------------------------------"
