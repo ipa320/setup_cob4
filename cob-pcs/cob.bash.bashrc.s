05bde984743902ebd2b32be51ae6e116ea867f43
ipa-fxm <felixmessmer@gmail.com>
2017-11-29 10:41:03 +0100

    unique color for sensoring bash

--- cob-pcs/cob.bash.bashrc.s
+++ cob-pcs/cob.bash.bashrc.s
@@ -3,4 +3,4 @@ function parse_git_branch () {
 }
 
 # set color fo all users
-PS1='${debian_chroot:+($debian_chroot)}\[\033[01;34m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[0;33m\]$(parse_git_branch)\[\033[0m\]\$ '
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[0;33m\]$(parse_git_branch)\[\033[0m\]\$ '
