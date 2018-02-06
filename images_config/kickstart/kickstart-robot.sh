9dd3c5fda48d97e18a73db7a809f28fda5b65ae6
Benjamin Maidel <bnm@ipa.fhg.de>
2018-02-06 18:22:39 +0100

    further improvements to kickstart config

--- images_config/kickstart/kickstart-robot.sh
+++ images_config/kickstart/kickstart-robot.sh
@@ -1,81 +1,55 @@
 #!/bin/bash
 
 function UpgradeAptPackages {
-    apt-get upgrade -y --force-yes
-}
-
-function SetupGroup {
-    if grep -q GRUB_RECORDFAIL_TIMEOUT= /etc/default/grub ; then
-        echo "found GRUB_RECORD_FAIL flag already, skipping SetupGroup (update-grub call)"
-    else
-        echo "GRUB_RECORDFAIL_TIMEOUT=10" >> /etc/default/grub
-        update-grub
-    fi
+    apt-get update
+    apt-get upgrade -y 
 }
 
 # TODO: fxm
 function UpgradeKernel {
-    apt-get install --install-recommends linux-generic-lts-xenial -y --allow
+    if [ "$DISTRO" == "trusty" ]; then
+        apt-get install --install-recommends linux-generic-lts-xenial -y --allow
+    fi
 }
 
-function InstallGraphicalIface {
+function InstallUbuntuGnome {
+    add-apt-repository ppa:gnome3-team/gnome3 -y
+    apt-get update
     apt-get install ubuntu-gnome-desktop -y
 }
 
-#TODO: fxm
 function InstallHWEnableStacks {
-    apt-get install --install-recommends linux-generic-lts-xenial xserver-xorg-core-lts-xenial xserver-xorg-lts-xenial xserver-xorg-video-all-lts-xenial xserver-xorg-input-all-lts-xenial libwayland-egl1-mesa-lts-xenial -y
-    dpkg-reconfigure xserver-xorg-lts-xenial
+    if [ "$DISTRO" == "trusty" ]; then
+        apt-get install --install-recommends linux-generic-lts-xenial xserver-xorg-core-lts-xenial xserver-xorg-lts-xenial xserver-xorg-video-all-lts-xenial xserver-xorg-input-all-lts-xenial libwayland-egl1-mesa-lts-xenial -y
+        dpkg-reconfigure xserver-xorg-lts-xenial
+    fi
 }
 
 function AddUsers {
     if [ "$INSTALL_TYPE" == "master" ]; then
-        echo "session	required	pam_mkhomedir.so " >> /etc/pam.d/common-session
+        echo "session required pam_mkhomedir.so " >> /etc/pam.d/common-session
 
-        # robot user
+        #Add robot user
         adduser --disabled-password --gecos "" robot --home /u/robot
-        echo "robot:sawVsPPn2.KXM"  | chpasswd -e
+        echo "robot:$1$.8rMo3Kc$hwkXrTTshYmLa9iplJchz."  | chpasswd -e
         usermod -m -d /u/robot robot
         cp -rT /etc/skel /u/robot/
         chown robot:robot /u/robot/
-    fi
-    # already done in seed @fxm
-    # # robot-local user
-    # sed -i 's/HOME=/home/HOME=/iscsi/user/g' /etc/default/useradd
-    # adduser --disabled-password --gecos "" robot-local --home /home/robot-local
-    # echo "robot-local:sawVsPPn2.KXM"  | chpasswd -e
-    # cp -rT /etc/skel /home/robot-local
-    # chown robot-local:robot-local /home/robot-local
-}
-#TODO password is not set correctly!!!!
-function SetupRootUser {
-    if grep -q "Defaults rootpw" /etc/sudoers ; then
-        echo "found Defaults rootpw in sudoers already, skipping SetupRootUser"
-    else
-        echo "Defaults rootpw" >> /etc/sudoers
-        usermod --password sawVsPPn2.KXM root 
-    fi
-}
 
-function GiveFullRights {
+        #Give robot user full rights for sudo
+        if grep -q "robot ALL=(ALL) NOPASSWD: ALL" /etc/sudoers ; then
+            echo "found robot NOPASSWD in sudoers already, skipping GiveFullRights to robot"
+        else
+            echo "robot ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+        fi
+    fi
+
+    #Give robot-local full sudo rights
     if grep -q "robot-local ALL=(ALL) NOPASSWD: ALL" /etc/sudoers ; then
         echo "found robot-local NOPASSWD in sudoers already, skipping GiveFullRights to robot-local"
     else
         echo "robot-local ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
     fi
-
-    if grep -q "robot ALL=(ALL) NOPASSWD: ALL" /etc/sudoers ; then
-        echo "found robot NOPASSWD in sudoers already, skipping GiveFullRights to robot"
-    else
-        echo "robot ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
-    fi
-}
-
-#TODO: requires user input
-function KeyboardLayout {
-    L='de' && sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
-    apt-get install console-data -y -f
-    dpkg-reconfigure keyboard-configuration
 }
 
 function NFSSetup {
@@ -106,17 +80,31 @@ function NFSSetup {
     fi
 }
 
-function ChronySetup {
-    apt-get install chrony -y -f
-    if [ "$INSTALL_TYPE" == "master" ]; then
-        wget -O /etc/chrony/chrony.conf https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/chrony_server
+function InstallROS {
+    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $DISTRO main" > /etc/apt/sources.list.d/ros-latest.list'
+    apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
+    apt-get update
+    apt-get install ros-kinetic-ros-base -y
+    apt-get install python-rosinstall python-rosinstall-generator python-wstool -y
+    apt-get install python-catkin-tools -y
+    apt-get install python-pip -y
+    apt-get install ros-kinetic-care-o-bot-robot
+}
 
-    elif [ "$INSTALL_TYPE" == "slave" ]; then
-        HOSTNAME=$(cat /etc/hostname)
-        SERVERNAME=$(echo ${HOSTNAME%-*}-b1)
-        wget -O /etc/chrony/chrony.conf https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/chrony_client
-        sed -i "s/server_ip/${SERVERNAME}/g" /etc/chrony/chrony.conf
+function SetupGrubRecFail {
+    if grep -q GRUB_RECORDFAIL_TIMEOUT= /etc/default/grub ; then
+        echo "found GRUB_RECORD_FAIL flag already, skipping SetupGrubRecFail (update-grub call)"
+    else
+        echo "GRUB_RECORDFAIL_TIMEOUT=2" >> /etc/default/grub
+        update-grub
     fi
+}
+
+#TODO: requires user input - No working solution found so far
+function KeyboardLayout {
+    L='de' && sed -i 's/XKBLAYOUT=\"\w*"/XKBLAYOUT=\"'$L'\"/g' /etc/default/keyboard
+    apt-get install console-data -y -f
+    dpkg-reconfigure keyboard-configuration
 }
 
 function ConfigureSSH {
@@ -130,9 +118,41 @@ function ConfigureSSH {
         echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
     fi
     sed -i 's/PermitRootLogin without-password/PermitRootLogin yes/g' /etc/ssh/sshd_config
-    service ssh restart
+    systemctl restart ssh
 }
 
+function InstallAptCacher {
+    HOSTNAME=$(cat /etc/hostname)
+    SERVERNAME=$(echo ${HOSTNAME%-*}-b1)
+    if [ "$INSTALL_TYPE" == "master" ]; then
+        apt-get install apt-cacher-ng -y
+        sed -i 's/\# PassThroughPattern: .\*/PassThroughPattern: .\*/g' /etc/apt-cacher-ng/acng.conf
+        systemctl restart apt-cacher-ng
+    elif [ "$INSTALL_TYPE" == "slave" ]; then
+        touch /etc/apt/apt.conf.d/01proxy
+
+        if grep -q 'Acquire::http { Proxy "http://'$SERVERNAME':3142"; };' /etc/apt/apt.conf.d/01proxy ; then
+            echo "Proxy already in /etc/apt/apt.conf.d/01proxy, skipping InstallAptCacher"
+        else
+            echo 'Acquire::http { Proxy "http://'$SERVERNAME':3142"; };' >>  /etc/apt/apt.conf.d/01proxy
+        fi
+    fi
+}
+
+function ChronySetup {
+    apt-get install chrony -y -f
+    if [ "$INSTALL_TYPE" == "master" ]; then
+        wget -O /etc/chrony/chrony.conf https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/chrony_server
+
+    elif [ "$INSTALL_TYPE" == "slave" ]; then
+        HOSTNAME=$(cat /etc/hostname)
+        SERVERNAME=$(echo ${HOSTNAME%-*}-b1)
+        wget -O /etc/chrony/chrony.conf https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/chrony_client
+        sed -i "s/server_ip/${SERVERNAME}/g" /etc/chrony/chrony.conf
+    fi
+}
+
+#udev rules 
 function SetupUdevRules {
     wget -O /etc/udev/rules.d/98-led.rules https://raw.githubusercontent.com/ipa320/setup_cob4/master/udev_rules/98-led.rules
     if [ "$INSTALL_TYPE" == "master" ]; then
@@ -144,21 +164,8 @@ function SetupUdevRules {
     fi
 }
 
-# TODO: fxm ;)
-function InstallROS {
-    sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-    apt-key adv --keyserver hkp://ha.pool.sks-keyservers.net:80 --recv-key 421C365BD9FF1F717815A3895523BAEEB01FA116
-    apt-get update
-    apt-get install ros-kinetic-ros-base -y
-    apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential -y
-    #apt-get install python-catkin-tools -y
-    #apt-get install python-pip -y
-    apt-get install ros-kinetic-care-o-bot-robot
-}
-
 function InstallGitLFS {
     curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
-    apt-get update
     apt-get install git-lfs
 }
 
@@ -197,37 +204,23 @@ function InstallShutdown {
 
 # TODO: warum ausloggen???
 function NetworkSetup {
-    apt-get -y remove biosdevname -y --force-yes
-    update-initramfs -u
-    update-grub
-    if [ "$INSTALL_TYPE" == "master" ]; then
-        (
-        wget -O /etc/network/interfaces https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/networkInterfacesMaster
-        ) 2>&1 | /usr/bin/tee /root/post-install.log
-    elif [ "$INSTALL_TYPE" == "slave" ]; then
-        (
-        wget -O /etc/network/interfaces https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/networkInterfacesSlave
-        ) 2>&1 | /usr/bin/tee /root/post-install.log
-    fi
-
-    chvt 1
-    touch /etc/network/interfaces.orig
-    cp /etc/network/interfaces /etc/network/interfaces.orig
-    echo "mv /etc/network/interfaces.orig /etc/network/interfaces && ifup -a && sed -i '/fixnet.sh/d' /etc/rc.local && rm -f /fixnet.sh" > /fixnet.sh
-    sed -i '/exit 0/ibash /fixnet.sh' /etc/rc.local
+    INTERFACE=`ip link | awk -F: '$0 !~ "lo|vir|wl|^[^0-9]"{print $2;getline}'`
 
     if [ "$INSTALL_TYPE" == "master" ]; then
-        wget -O /etc/network/interfaces https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/networkInterfacesMaster
+        wget -O /etc/network/interfaces https://raw.githubusercontent.com/ipa-bnm/setup_cob4/feature/xenial_unattended/cob-pcs/networkInterfacesMaster
     elif [ "$INSTALL_TYPE" == "slave" ]; then
-        wget -O /etc/network/interfaces https://raw.githubusercontent.com/ipa320/setup_cob4/master/cob-pcs/networkInterfacesSlave
+        wget -O /etc/network/interfaces https://raw.githubusercontent.com/ipa-bnm/setup_cob4/feature/xenial_unattended/cob-pcs/networkInterfacesSlave
     fi
-    /etc/init.d/networking restart
+
+    sed -i "s/eth0/$INTERFACE/g" /etc/network/interfaces
+
+    systemctl restart networking
 }
 
 function SetupEtcHosts {
     HOSTNAME=$(cat /etc/hostname)
 
-    sed -i "s/$HOSTNAME.ipa-apartment.org\t//g" /etc/hosts
+    sed -i "s/$HOSTNAME.wlrob.net\t//g" /etc/hosts
 
     ROBOTNAME=$(echo ${HOSTNAME%-*})
     ROBOT_NUM=$(echo ${ROBOTNAME##*-})
@@ -251,14 +244,9 @@ function SetupEtcHosts {
     sed -i $'8 a \n' /etc/hosts
 }
 
-# TODO: fxm ;)
 function InstallCandumpTools {
-    initctl stop candump2mongodb
-    rm /etc/init/candump2mongodb.conf
     wget -O /usr/local/bin/socket_buffer.py https://raw.githubusercontent.com/ipa320/setup_cob4/master/scripts/socket_buffer.py
     chmod +x /usr/local/bin/socket_buffer.py
-    #wget -O /etc/init/candump_any.conf https://raw.githubusercontent.com/ipa320/setup_cob4/master/scripts/candump_any.conf
-    #initctl start candump_any
 }
 
 #TODO: only newest NoMachine ?!?
@@ -268,11 +256,13 @@ function InstallNoMachine {
     dpkg -i /root/nomachine_${NOMACHINE_VERSION}_amd64.deb
 }
 
-function NetDataTools {
+function InstallNetData {
     apt-get install zlib1g-dev uuid-dev libmnl-dev gcc make git autoconf autoconf-archive autogen automake pkg-config curl -y
     git clone https://github.com/firehol/netdata.git --depth=1 /root/netdata
     cd /root/netdata
     yes | ./netdata-installer.sh
+    cd
+    rm -r /root/netdata
 }
 
 function InstallCobCommand {
@@ -292,68 +282,57 @@ function DisableUpdatePopup {
 }
 
 # TODO: fxm ;)
+# Not functional under xenial
 function DisableFailsafeBoot {
     sed -i 's/start on \(filesystem and static-network-up\) or failsafe-boot/start on filesystem and static-network-up/g' /etc/init/rc-sysinit.conf
 }
-
-function InstallAptCacher {
-    apt-get install apt-cacher-ng -y
-    HOSTNAME=$(cat /etc/hostname)
-    SERVERNAME=$(echo ${HOSTNAME%-*}-b1)
-    #TODO is $SERVERNAME same as server_ip???
-    touch /etc/apt/apt.conf.d/01proxy
-    if grep -q 'Acquire::http { Proxy "http://'$SERVERNAME':3142"; };' /etc/apt/apt.conf.d/01proxy ; then
-        echo "Proxy already in /etc/apt/apt.conf.d/01proxy, skipping InstallAptCacher"
-    else
-        echo 'Acquire::http { Proxy "http://server_ip:3142"; };' >>  /etc/apt/apt.conf.d/01proxy
-    fi
-    sed -i "s/server_ip/${SERVERNAME}/g" /etc/apt/apt.conf.d/01proxy
-    if [ "$INSTALL_TYPE" == "master" ]; then
-        sed -i 's/\# PassThroughPattern: .\*/PassThroughPattern: .\*/g' /etc/apt-cacher-ng/acng.conf
-    fi
-}
-
 
 ########################################################################
 ############################# INITIAL MENU #############################
 ########################################################################
 
-if [ $# -ne 1 ]; then
+if [ $# -ne 2 ]; then
     echo "ERROR: wrong number of arguments, expecting:"
-    echo "kickstart-robot.sh [master|slave]"
+    echo "kickstart-robot.sh [master|slave] [xenial|trusty]"
     exit 1
 fi
 
 if [ "$1" != "master" ] && [ "$1" != "slave" ]; then
-    echo "ERROR: please provide argument [master|slave]. Got: $1"
+    echo "ERROR: please provide argument [master|slave] [xenial|trusty]. Got: $1 $2"
+    exit 2
+elif [ "$2" != "xenial" ] && [ "$2" != "trusty" ]; then
+    echo "ERROR: please provide argument [master|slave] [xenial|trusty]. Got: $1 $2"
     exit 2
 else
     INSTALL_TYPE=$1
-    echo "Starting kickstart robot - using install type: $INSTALL_TYPE"
+    DISTRO=$2
+    echo "Starting kickstart robot - using install type: $INSTALL_TYPE for distro $DISTRO"
 fi
 
 UpgradeAptPackages
-SetupGroup
-#UpgradeKernel
-InstallGraphicalIface
-#InstallHWEnableStacks
+UpgradeKernel
+InstallUbuntuGnome
+InstallHWEnableStacks
 AddUsers
-SetupRootUser
-GiveFullRights
-#KeyboardLayout
 NFSSetup
-ChronySetup
+InstallROS
+SetupGrubRecFail
+#KeyboardLayout
 ConfigureSSH
+
+unset http_proxy
+InstallAptCacher
+ChronySetup
 SetupUdevRules
-#InstallROS
 InstallGitLFS
 SetupDefaultBashEnv
 InstallShutdown
 NetworkSetup
 SetupEtcHosts
-#InstallCandumpTools
+InstallCandumpTools
 InstallNoMachine
+InstallNetData
 InstallCobCommand
 RemoveModemanager
+DisableUpdatePopup
 #DisableFailsafeBoot
-InstallAptCacher
