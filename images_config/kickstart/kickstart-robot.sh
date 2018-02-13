cb53ff342f539ac45b6604887c75c17b9f66edb4
Benjamin Maidel <bnm@ipa.fhg.de>
2018-02-13 15:43:45 +0100

    fixed ssh configureation

--- images_config/kickstart/kickstart-robot.sh
+++ images_config/kickstart/kickstart-robot.sh
@@ -171,22 +171,22 @@ function ConfigureSSH {
     printHeader "ConfigureSSH"
     apt-get install openssh-server -y
 
-    if grep -q "X11Forwarding"; then
+    if grep -q "X11Forwarding" /etc/ssh/sshd_config; then
         sed -i 's/X11Forwarding.*$/X11Forwarding yes/g' /etc/ssh/sshd_config
     else
         echo "X11Forwarding yes" >> /etc/ssh/sshd_config
     fi
-    if grep -q "X11UseLocalhost"; then
+    if grep -q "X11UseLocalhost" /etc/ssh/sshd_config; then
         sed -i 's/X11UseLocalhost.*$/X11UseLocalhost no/g' /etc/ssh/sshd_config
     else
         echo "X11UseLocalhost no" >> /etc/ssh/sshd_config
     fi
-    if grep -q "PermitRootLogin"; then
+    if grep -q "PermitRootLogin" /etc/ssh/sshd_config; then
         sed -i 's/PermitRootLogin.*$/PermitRootLogin yes/g' /etc/ssh/sshd_config
     else
         echo "PermitRootLogin yes">> /etc/ssh/sshd_config
     fi
-    if grep -q "PermitRootLogin"; then
+    if grep -q "PermitRootLogin" /etc/ssh/sshd_config; then
         sed -i 's/ClientAliveInterval.*$/ClientAliveInterval 60/g' /etc/ssh/sshd_config
     else
         echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
