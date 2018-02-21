3ce7141976f5c286fc738acaa550b7f7a70d9c01
cob4-18 <cob4-18@ipa.fhg.de>
2018-02-21 09:26:35 +0100

    remove domain from hostnames

--- helper_client_list.sh
+++ helper_client_list.sh
@@ -3,9 +3,9 @@
 #outsources retrieval client_list variables
 IP=$(hostname -I | awk '{print $1}')
 client_list_ip=$(nmap --unprivileged $IP-50 --system-dns | grep report | awk '{print $6}' | sed 's/(//g;s/)//g' | tr '\n' ' ')
-client_list_hostnames=$(nmap --unprivileged $IP-50 --system-dns | grep report | awk '{print $5}' | sed 's/(//g;s/)//g' | tr '\n' ' ')
+client_list_hostnames=$(nmap --unprivileged $IP-50 --system-dns | grep report | awk '{print $5}' | sed 's/(//g;s/)//g' | sed 's/\..*$//g' | tr '\n' ' ')
 
-#echo client_list_ip
-#echo $client_list_ip
-#echo client_list_hostnames
-#echo $client_list_hostnames
+echo client_list_ip
+echo $client_list_ip
+echo client_list_hostnames
+echo $client_list_hostnames
